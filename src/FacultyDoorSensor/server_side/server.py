0e0d54e81536e6d249b74ff83254815d536e1bed
Riley Kirkpatrick <kirkpatrickr@moravian.edu>
2020-11-16 21:15:05 -0500

    Remove root endpoint for webserver use

--- src/FacultyDoorSensor/server_side/server.py
+++ src/FacultyDoorSensor/server_side/server.py
@@ -15,39 +15,6 @@ def create_server():
     '''Create server, add endpoints, and return the server'''
     server = Server()
 
-    @server.app.route('/')
-    def website():
-        try:
-            statuses = []
-            for name in server.doors_status.doors:
-                logging.info(f'Attempting to get status of door: {name}')
-                statuses.append((name, server.doors_status.get_status(name)))
-                logging.info(f'Received status of door: {name}')
-            with open('base.html', 'r') as html:
-                text = html.read()
-            head, divs, end = text.strip().split('\n\n')
-            divs = divs.split('\n')
-            for i in range(len(divs)):
-                name, status = statuses[i]
-                if status == 'CLOSED':
-                    color = 'red'
-                elif status == 'OPEN':
-                    color = 'green'
-                else:
-                    color = 'orange'
-                    status = 'NULL'
-                divs[i].replace('---', color, 1)
-                divs[i].replace('---', name, 1)
-                divs[i].replace('---', status, 1)
-            divs = '\n'.join(divs)
-            website = f'{head}\n{divs}\n{end}'
-            return website, 200
-        except IOError:
-            logging.error('There was an issue when opening the base.html file.')
-        except Exception:
-            logging.error('An unexpected issue occurred while attempting to retrieve the website.')
-        return "Website not retrieved", 404
-
     @server.app.route('/get_status')
     def get_status():
         # Parameters are passed at the end of the URL as ?key=foo
